a58daba5f0fbe75de4a9ff5828ae961ee90ecd84
has0825 <k024g0026@g.neec.ac.jp>
2026-04-21 12:54:37 +0900

    修正

--- project/Resources/shaders/Object3d.PS.hlsl
+++ project/Resources/shaders/Object3d.PS.hlsl
@@ -6,6 +6,7 @@ ConstantBuffer<DirectionalLight> gDirectionalLight : register(b1);
 ConstantBuffer<Camera> gCamera : register(b2);
 
 Texture2D<float32_t4> gTexture : register(t0);
+TextureCube<float32_t4> gEnvironmentTexture : register(t1); // 環境マップ用
 SamplerState gSampler : register(s0);
 
 struct PixelShaderOutput
@@ -17,11 +18,13 @@ PixelShaderOutput main(VertexShaderOutput input)
 {
     PixelShaderOutput output;
     
+    // UVトランスフォームの適用
     float4 transformedUV = mul(float32_t4(input.texcoord, 0.0f, 1.0f), gMaterial.uvTransform);
     float32_t4 textureColor = gTexture.Sample(gSampler, transformedUV.xy);
     
     if (gMaterial.enableLighting != 0)
     {
+        // Punctual Light (Directional Light) の計算
         float32_t3 normal = normalize(input.normal);
         float32_t3 lightDir = normalize(-gDirectionalLight.direction);
         float NdotL = dot(normal, lightDir);
@@ -30,6 +33,17 @@ PixelShaderOutput main(VertexShaderOutput input)
 
         output.color.rgb = diffuse;
         output.color.a = gMaterial.color.a * textureColor.a;
+
+        // 環境マップ (Environment Map) の計算
+        // カメラからピクセルへのベクトル
+        float32_t3 cameraToPosition = normalize(input.worldPosition - gCamera.worldPosition);
+        // 法線を使ってベクトルを反射
+        float32_t3 reflectedVector = reflect(cameraToPosition, normal);
+        // 環境マップからサンプリング
+        float32_t4 environmentColor = gEnvironmentTexture.Sample(gSampler, reflectedVector);
+        
+        // 映り込みの強さを掛けて加算
+        output.color.rgb += environmentColor.rgb * gMaterial.environmentCoefficient;
     }
     else
     {
